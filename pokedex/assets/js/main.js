d3b09e12dd60c437070719d18afcfa0eaf5e51ee
GabryellPontes <lucaas.gabryell17@gmail.com>
2022-11-19 23:05:39 -0300

    Criando arquivo separado só para requisição

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -24,15 +24,15 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-	.then((reponse) => reponse.json())
-	.then((jsonBody) => jsonBody.results)
-	.then((pokemons) => {
+pokeApi.getPokemons().then( (pokemons) => { 
+	const listItens = []
 
 		for (let i = 0; i < pokemons.length; i++) {
 			const pokemon = pokemons[i];
-			pokemonList.innerHTML += convertPokemonToLi(pokemon)
+		 listItens.push(convertPokemonToLi(pokemon))
+		 
 		}
-
+		
+		console.log (listItens)
+	
 	})
-	.catch((error) => console.log(error))
